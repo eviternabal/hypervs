07055a2c374213dd7565424d6e6b53b8826c641a
eliasbrucart <eliasbrucart@hardgames.com.ar>
2022-06-27 01:14:07 -0300

    Applied new movement system

--- Assets/Scripts/Gameplay/ShipController.cs
+++ Assets/Scripts/Gameplay/ShipController.cs
@@ -9,13 +9,36 @@ namespace NAMESPACENAME.Gameplay.Ship
         [SerializeField] float forwardSpeed;
         [Header("Runtime Values")]
         [SerializeField] Vector3 moveInput;
+        [SerializeField] private float timeToDecrementForward;
+        [SerializeField] private float decrementForwardValue;
+
+        private float timerDecrementForward;
+
 
         public System.Action ShipMoved;
         public System.Action ShipAdvanced;
 
+        private void Start()
+        {
+            Ring.IncrementSpeed += IncrementForwardSpeed;
+        }
+
+        private void OnDisable()
+        {
+            Ring.IncrementSpeed -= IncrementForwardSpeed;
+        }
+
         //Unity Events
         private void Update()
         {
+            if (timerDecrementForward < timeToDecrementForward)
+                timerDecrementForward += Time.deltaTime;
+            else
+            {
+                forwardSpeed -= decrementForwardValue;
+                timerDecrementForward = 0.0f;
+            }
+
             MoveForward();
 
             if (moveInput.magnitude > 0)
@@ -51,6 +74,11 @@ namespace NAMESPACENAME.Gameplay.Ship
         public void GetInputY(float input)
         {
             moveInput.y = input * XYSpeed;
+        }
+
+        public void IncrementForwardSpeed(float value)
+        {
+            forwardSpeed += value;
         }
     }
 }
